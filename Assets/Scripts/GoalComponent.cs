803bbd398c1513b770c57ba4b21530dbe8ad9ab8
Sam <samlow1218@gmail.com>
2019-03-12 20:28:36 -0400

    Added goals(scoring)

---
+++ Assets/Scripts/GoalComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalComponent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Goal Scored");
+    }
+}
